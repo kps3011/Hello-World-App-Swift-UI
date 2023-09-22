b6b4425402bf1515a4e0c954cd05fd93d5bc48a2
Kaushlendra Singh <kaushlendrapratap3011@gm>
2023-09-22 23:13:01 +0530

    Initial Commit

---
+++ Hello World App Swift UI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Hello World App Swift UI
+//
+//  Created by Kaushlendra Singh on 22/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello World App Swift UI/Hello_World_App_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_World_App_Swift_UIApp.swift
+//  Hello World App Swift UI
+//
+//  Created by Kaushlendra Singh on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_World_App_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
